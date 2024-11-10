f330df69950f17498de225e023c3a56fc56e8c7d
Jhuly Vivas <131303835+JhulyVivas@users.noreply.github.com>
2024-11-10 18:50:12 -0500

    FEAT: Prometheus, node_exporter y grafana

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,9 @@ Vagrant.configure("2") do |config|
   servidor.vm.box = "bento/ubuntu-22.04"
   servidor.vm.network :private_network, ip: "192.168.50.2"
   servidor.vm.provision "file", source: "dockerWebapp", destination: "/home/vagrant/"
+  servidor.vm.provision "file", source: "prometheus.yml", destination: "/home/vagrant/"
+  servidor.vm.provision "file", source: "prometheus.service", destination: "/home/vagrant/"
+  servidor.vm.provision "file", source: "node_exporter.service", destination: "/home/vagrant/"
   servidor.vm.provision "shell", path: "script.sh"
   servidor.vm.hostname = "servidor"
   end
--- script.sh
+++ script.sh
@@ -25,6 +25,59 @@ sudo apt install -y docker-ce
 # su - ${USER}
 # id -nG
 
+echo "installing prometheus"
+sudo groupadd --system prometheus
+sudo useradd -s /sbin/nologin --system -g prometheus prometheus
+
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+
+wget https://github.com/prometheus/prometheus/releases/download/v2.43.0/prometheus-2.43.0.linux-amd64.tar.gz
+
+tar vxf prometheus*.tar.gz
+
+cd prometheus*/
+
+sudo mv prometheus /usr/local/bin
+sudo mv promtool /usr/local/bin
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+
+sudo mv consoles /etc/prometheus
+sudo mv console_libraries /etc/prometheus
+sudo mv prometheus.yml /etc/prometheus
+
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+cd /home/vagrant
+sudo cp prometheus.yml /etc/prometheus/prometheus.yml
+sudo cp prometheus.service /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+
+echo "installing node_exporter"
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xvfz node_exporter-1.8.2.linux-amd64.tar.gz
+sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
+sudo mv node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin/
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+echo "installing grafana"
+sudo apt-get install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update -y
+sudo apt-get install -y grafana
+sudo systemctl start grafana-server
+
+echo "Start docker webapp"
 cd /home/vagrant/dockerWebapp
 sudo docker compose -f docker-compose.yml up -d
 sudo docker compose ps
